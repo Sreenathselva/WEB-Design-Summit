6bc8cddbfe5a0ef9cb2b5f4bf5461b05d7e87ec8
Sreenathselva <114873185+Sreenathselva@users.noreply.github.com>
2024-01-25 13:17:52 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,63 @@
+const navEl=document.querySelector('.navbar-container')
+window.addEventListener('scroll' ,( ) =>{
+if(window.scrollY >= 56){
+  navEl.classList.add('navbar-scrolled');
+}
+else if(window.scrollY <=56){
+  navEl.classList.remove('navbar-scrolled');
+}
+})
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            // Remove active class from all links
+            navLinks.forEach(function (navLink) {
+                navLink.classList.remove('navActive');
+            });
+
+            // Add active class to the clicked link
+            link.classList.add('navActive');
+        });
+    });
+});
+
+// COUNTDOWN
+ // Set the date we're counting down to
+ var countDownDate = new Date("2024-06-10T07:59:59+04:00").getTime();
+  
+ // Update the count down every 1 second
+ var x = setInterval(function() {
+ 
+   // Get today's date and time
+   var now = new Date().getTime();
+ 
+   // Find the distance between now and the count down date
+   var distance = countDownDate - now;
+ 
+   // Time calculations for days, hours, minutes and seconds
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ 
+   // Display the result in the element with id="demo"
+   document.getElementById("d").innerHTML = days;
+   document.getElementById("h").innerHTML =hours;
+   document.getElementById("m").innerHTML = minutes;
+   document.getElementById("s").innerHTML = seconds;
+     // If the count down is finished, write some text
+     if (distance < 0) {
+       clearInterval(x);
+       document.getElementById("demo").innerHTML = "EXPIRED";
+     }
+   }, 1000);
+ 
 
 
 document.addEventListener("DOMContentLoaded", function() {
